b0be31181378595894ed3d01765e048e7e8e7409
hung.vm.dev <hungvumanh2001@gmail.com>
2022-08-14 10:50:40 +0700

    first commit

---
+++ src/main/java/com/example/metronicshop/MetronicShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.metronicshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetronicShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetronicShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/metronicshop/MetronicShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metronicshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetronicShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
