413e47eeb02509d0961710cdd4c71a1d32fe1564
hung.vm.dev <hungvumanh2001@gmail.com>
2022-08-26 17:22:33 +0700

    done delete item in cart

--- src/main/java/com/example/metronicshop/controller/CartController.java
+++ src/main/java/com/example/metronicshop/controller/CartController.java
@@ -16,116 +16,114 @@ import java.util.logging.Logger;
 @CrossOrigin("*")
 @RequestMapping("/carts")
 public class CartController {
-    private static Logger logger=Logger.getLogger(String.valueOf(CartController.class));
+    private static Logger logger = Logger.getLogger(String.valueOf(CartController.class));
     @Autowired
     private CartService cartService;
     @Autowired
     private CartRepository cartRepository;
+
     //lấy ra sản phẩm của người dùng
     @GetMapping
-    public ResponseEntity<Cart>findByProductId(@RequestParam Long productId,@RequestParam Long userId)
-    {
-        Cart cart=cartRepository.findByProductIdAndUserId(productId,userId);
-        if(cart==null)
+    public ResponseEntity<Cart> findByProductId(@RequestParam Long productId, @RequestParam Long userId) {
+        Cart cart = cartRepository.findByProductIdAndUserId(productId, userId);
+        if (cart == null)
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         else
-            return new ResponseEntity<>(cart,HttpStatus.OK);
+            return new ResponseEntity<>(cart, HttpStatus.OK);
     }
+
     @GetMapping("/{productId}")
-    public ResponseEntity<Cart> findByProductId(@RequestParam Long productId)
-    {
-        return new ResponseEntity(cartRepository.findAllByProductId(productId),HttpStatus.OK);
+    public ResponseEntity<Cart> findByProductId(@RequestParam Long productId) {
+        return new ResponseEntity(cartRepository.findAllByProductId(productId), HttpStatus.OK);
     }
+
+    @GetMapping("/search-by-cartId/{cartId}")
+    public ResponseEntity<Cart> findByCartId(@PathVariable Long cartId) {
+        return new ResponseEntity(cartRepository.findById(cartId), HttpStatus.OK);
+    }
+
     @GetMapping("/search-by-userId/{userId}")
-    public ResponseEntity<Cart> findAllByUserId(@PathVariable Long userId)
-    {
-        return new ResponseEntity(cartRepository.findAllByUserId(userId),HttpStatus.OK);
+    public ResponseEntity<Cart> findAllByUserId(@PathVariable Long userId) {
+        return new ResponseEntity(cartRepository.findAllByUserId(userId), HttpStatus.OK);
     }
 
     @PostMapping("/add-cart")
-    public ResponseEntity findAllByStatus(@RequestBody Cart cart)
-    {
+    public ResponseEntity findAllByStatus(@RequestBody Cart cart) {
         cartService.save(cart);
         logger.info("Save product to cart");
         return new ResponseEntity(cart, HttpStatus.OK);
     }
+
     @PutMapping("/save-number-product")
-    public ResponseEntity insertProductNuber(@RequestParam Long productId,@RequestParam Long userId)
-    {
-        Cart cart=cartRepository.findByProductIdAndUserId(productId,userId);
-        if(cart==null)
+    public ResponseEntity insertProductNuber(@RequestParam Long productId, @RequestParam Long userId) {
+        Cart cart = cartRepository.findByProductIdAndUserId(productId, userId);
+        if (cart == null)
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        else
-        {
-            cart.setNumber(cart.getNumber()+1);
+        else {
+            cart.setNumber(cart.getNumber() + 1);
             cartRepository.save(cart);
             logger.info("Insert successful!");
             return new ResponseEntity(HttpStatus.OK);
         }
     }
+
     @GetMapping("/search-by-status")
-    public ResponseEntity<Iterable<Cart>> findAllByStatus(@RequestParam Long id, @RequestParam int status)
-    {
-        Iterable<Cart> carts=cartService.getNumberCart(id,status);
-        if(carts==null)
+    public ResponseEntity<Iterable<Cart>> findAllByStatus(@RequestParam Long id, @RequestParam int status) {
+        Iterable<Cart> carts = cartService.getNumberCart(id, status);
+        if (carts == null)
             return new ResponseEntity<>(carts, HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(carts, HttpStatus.OK);
     }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteCart(@PathVariable Long id)
-    {
-        boolean check= cartRepository.existsById(id);//kiểm tra xem id này có không
-        if(check){
+    public ResponseEntity deleteCart(@PathVariable Long id) {
+        boolean check = cartRepository.existsById(id);//kiểm tra xem id này có không
+        if (check) {
             cartService.deleteCart(id);
             logger.info("delete cart by id");
             return new ResponseEntity(HttpStatus.OK);
-        }
-        else
+        } else
             return new ResponseEntity(HttpStatus.NOT_FOUND);
     }
+
     @GetMapping("/order")
-    public ResponseEntity<Iterable<Cart>>listOrder(@RequestParam Long userId,@RequestParam int status)
-    {
-        Iterable<Cart> carts=cartService.listUserOrder(userId,status);
-        if(carts==null)
-        {
+    public ResponseEntity<Iterable<Cart>> listOrder(@RequestParam Long userId, @RequestParam int status) {
+        Iterable<Cart> carts = cartService.listUserOrder(userId, status);
+        if (carts == null) {
             logger.info("not found list user order");
             return new ResponseEntity(HttpStatus.NOT_FOUND);
-        }
-        else
-        {
+        } else {
             logger.info("get list user order successfull!");
-            return new ResponseEntity(carts,HttpStatus.OK);
+            return new ResponseEntity(carts, HttpStatus.OK);
         }
     }
+
     @PutMapping("/change-status")
-    public ResponseEntity changeStatus(@RequestParam Long userId)
-    {
+    public ResponseEntity changeStatus(@RequestParam Long userId) {
         //đặt hàng thì tự nhảy vào đây
-        Iterable<Cart> carts=cartService.findAllByUserId(userId);
-        for (Cart i:carts) {
+        Iterable<Cart> carts = cartService.findAllByUserId(userId);
+        for (Cart i : carts) {
             i.setStatus(2);
             cartRepository.save(i);
         }
-        return new ResponseEntity(carts,HttpStatus.OK);
+        return new ResponseEntity(carts, HttpStatus.OK);
 
     }
+
     @PutMapping("/confirm")
-    public ResponseEntity confirmOrder(@RequestParam Long id)
-    {
+    public ResponseEntity confirmOrder(@RequestParam Long id) {
         Cart cart = cartRepository.findAllById(id);
         //nếu đã thêm vào giỏ thì chuyển thành đặt hàng
         //còn k thì chuyển thành xác nhận đơn hàng
-        cart.setStatus(cart.getStatus()==1?2:3);
+        cart.setStatus(cart.getStatus() == 1 ? 2 : 3);
         cartRepository.save(cart);
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @PutMapping("/update-cart/{id}")
-    public ResponseEntity updateCart(@PathVariable Long id, @RequestBody Cart cart)
-    {
+    public ResponseEntity updateCart(@PathVariable Long id, @RequestBody Cart cart) {
         Optional<Cart> oldCart = cartRepository.findById(id);
-        if(!oldCart.isPresent()){
+        if (!oldCart.isPresent()) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
         cart.setId(id);
