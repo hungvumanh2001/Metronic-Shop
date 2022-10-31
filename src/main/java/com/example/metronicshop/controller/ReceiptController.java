433e9828f5948db76d70feef0358e8af6b0624a2
hung.vm.dev <hungvumanh2001@gmail.com>
2022-10-31 22:55:23 +0700

    fix checkout

--- src/main/java/com/example/metronicshop/controller/ReceiptController.java
+++ src/main/java/com/example/metronicshop/controller/ReceiptController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Logger;
 
@@ -23,9 +20,11 @@ public class ReceiptController {
     ReceiptService receiptService;
 
     @PostMapping("/add-receipt")
-    public ResponseEntity findAllByStatus(@RequestBody Receipt receipt) {
+    public ResponseEntity save(@RequestBody Receipt receipt) {
         receiptService.save(receipt);
         logger.info("Save product to receipt");
         return new ResponseEntity(HttpStatus.OK);
     }
+
+
 }
